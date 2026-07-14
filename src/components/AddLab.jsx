789282da8bca6d3c3efbca5c50f89b71dd8fa597
Annmariya-git <annmariyavarghese2027@gmail.com>
2026-07-14 22:44:33 +0530

    Edited

--- src/components/AddLab.jsx
+++ src/components/AddLab.jsx
@@ -41,41 +41,41 @@ const AddLab = () => {
                     <div className="row">
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">id</label>
-                            <input type="num" className="form-control" name="donor_name" value={input.id} onChange={inputHandler}/>
+                            <input type="num" className="form-control" name="id" value={input.id} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">name</label>
-                            <input type="number" className="form-control" name="age" value={input.name} onChange={inputHandler}/>
+                            <input type="number" className="form-control" name="name" value={input.name} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">dept</label>
-                             <input type="number" className="form-control" name="gender" value={input.dept} onChange={inputHandler}/>
+                             <input type="number" className="form-control" name="dept" value={input.dept} onChange={inputHandler}/>
                             
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">sem</label>
-                             <input type="number" className="form-control" name="blood_group" value={input.sem} onChange={inputHandler}/>
+                             <input type="number" className="form-control" name="sem" value={input.sem} onChange={inputHandler}/>
     
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">course</label>
-                            <input type="num" className="form-control" name="phone" value={input.course} onChange={inputHandler}/>
+                            <input type="num" className="form-control" name="course" value={input.course} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">sysNo</label>
-                            <input type="email" className="form-control" name="email" value={input.sysNo} onChange={inputHandler}/>
+                            <input type="email" className="form-control" name="sysNo" value={input.sysNo} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">logTime</label>
-                            <input type="text" className="form-control" name="city" value={input.logTime} onChange={inputHandler}/>
+                            <input type="text" className="form-control" name="logTime" value={input.logTime} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">logoutTime</label>
-                            <input type="text" className="form-control" name="weight_kg" value={input.logoutTime} onChange={inputHandler}/>
+                            <input type="text" className="form-control" name="logoutTime" value={input.logoutTime} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">date</label>
-                            <input type="date" name="" id="" className="form-control" name="last_donation_date" value={input.date} onChange={inputHandler}/>
+                            <input type="date" name="" id="" className="form-control" name="date" value={input.date} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <button className="btn btn-success" onClick={readvalue}>Submit</button>
